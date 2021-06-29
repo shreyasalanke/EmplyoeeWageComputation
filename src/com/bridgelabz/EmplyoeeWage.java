689bff09a3321d13f67e5eab01c45c5ef0af21b7
shreyasalanke <shreyasalanke97@gmail.com>
2021-06-29 09:53:32 +0530

    ADDED: switch case statement to check emplyoee wage

--- src/com/bridgelabz/EmplyoeeWage.java
+++ src/com/bridgelabz/EmplyoeeWage.java
@@ -11,21 +11,22 @@ public class EmplyoeeWage {
 	public static void main(String[] args) {
 		int dailyWage=0;
 		double empCheck = Math.floor(Math.random()*10)%3;
-		if(empCheck == IS_EMP_PRESENT)
-		{
-			System.out.println("emplyoee is full time present");
-			dailyWage=FULL_DAY_HOUR * WAGE_PER_HOUR;
+		
+		 switch ((int)empCheck)
+		 {
+		  case IS_EMP_PRESENT:
+			  System.out.println("emplyoee is present full time");
+			  dailyWage=FULL_DAY_HOUR * WAGE_PER_HOUR;
+			  System.out.println("emplyoe wage is:"+dailyWage);
+			  break;
+		  case IS_PART_TIME:	  
+			  System.out.println("emplyoee is present part time");
+			  dailyWage=PART_TIME_HOUR * WAGE_PER_HOUR;
+			  System.out.println("emplyoe wage is:"+dailyWage);
+			  break;
+		default:
+			  System.out.println("emplyoee is absent");
+			break;
 		}
-		else if (empCheck == IS_PART_TIME)
-		{
-			System.out.println("emplyoee is present part time");
-			dailyWage=PART_TIME_HOUR *WAGE_PER_HOUR;
-		}
-		else
-		{
-			System.out.println("emplyoee is absent");
-		}
-		System.out.println("daily emplyoee wage is :"+dailyWage);
 	}
-
 }
