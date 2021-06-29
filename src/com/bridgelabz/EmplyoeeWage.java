72ee2bfb3f67d7e26d08f8c3e9b13d5cdac765cd
shreyasalanke <shreyasalanke97@gmail.com>
2021-06-29 10:33:55 +0530

    MODIFIED: caclualte wage till condition of total working hours or maximum days

--- src/com/bridgelabz/EmplyoeeWage.java
+++ src/com/bridgelabz/EmplyoeeWage.java
@@ -5,35 +5,43 @@ public class EmplyoeeWage {
 	public static final int IS_EMP_PRESENT=1;
 	public static final int IS_PART_TIME=2;
 	public static final int WAGE_PER_HOUR=20;
-	public static final int FULL_DAY_HOUR=8;
-	public static final int PART_TIME_HOUR=4;
 	public static final int NUMBER_OF_WORKING_DAYS=20;
+	public static final int MAX_WORK_HOURS=100;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		int totalWorkingDays=0;
+		int totalEmplyoeeHour=0;
 		int monthlyWage=0;
-		int dailyWage=0;
-		int day=0;
-		for(day=0; day <NUMBER_OF_WORKING_DAYS;day++)
+		int empHrs=0;
+	
+		while(totalEmplyoeeHour < MAX_WORK_HOURS && totalWorkingDays < NUMBER_OF_WORKING_DAYS )
 		{
+			
+		totalWorkingDays++;
+		int dailyWage=0;
+	    
 		double empCheck = Math.floor(Math.random()*10)%3;
 		
 		 switch ((int)empCheck)
 		 {
 		  case IS_EMP_PRESENT:
-			  System.out.println("emplyoee is present full time");
-			  dailyWage=FULL_DAY_HOUR * WAGE_PER_HOUR;
+			  empHrs=8;
 			  break;
 		  case IS_PART_TIME:	  
-			  System.out.println("emplyoee is present part time");
-			  dailyWage=PART_TIME_HOUR * WAGE_PER_HOUR;
+			  empHrs=4;
 			  break;
 		default:
+			  empHrs=0;
 			  System.out.println("emplyoee is absent");
-			break;
 		}
-		 monthlyWage= monthlyWage + dailyWage;
-		 System.out.println("emplyoe wage is:"+dailyWage);
-	  }
-		System.out.println("emplyoe wage per month:"+ monthlyWage);
-   }
+		 totalEmplyoeeHour= totalEmplyoeeHour + empHrs;
+			dailyWage = WAGE_PER_HOUR * empHrs ;
+			monthlyWage = monthlyWage + dailyWage ;
+			
+			System.out.println("Emplyoee Wage is : "+dailyWage);
+		}
+		System.out.println("monthly wage : " +monthlyWage);
+		System.out.println("total Hrs : " +totalEmplyoeeHour);
+	}
 }
