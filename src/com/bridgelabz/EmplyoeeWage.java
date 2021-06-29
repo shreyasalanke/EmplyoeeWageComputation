80b247eb83372e4ffd462df9abc60433e4cea0bb
shreyasalanke <shreyasalanke97@gmail.com>
2021-06-29 10:12:11 +0530

    ADDED: caclualte monthly wage

--- src/com/bridgelabz/EmplyoeeWage.java
+++ src/com/bridgelabz/EmplyoeeWage.java
@@ -2,14 +2,19 @@ package com.bridgelabz;
 
 public class EmplyoeeWage {
 	
-	public static final int IS_EMP_PRESENT= 1;
-	public static final int IS_PART_TIME= 2;
-	public static final int WAGE_PER_HOUR= 20;
-	public static final int FULL_DAY_HOUR= 8;
-	public static final int PART_TIME_HOUR= 4;
+	public static final int IS_EMP_PRESENT=1;
+	public static final int IS_PART_TIME=2;
+	public static final int WAGE_PER_HOUR=20;
+	public static final int FULL_DAY_HOUR=8;
+	public static final int PART_TIME_HOUR=4;
+	public static final int NUMBER_OF_WORKING_DAYS=20;
 	
 	public static void main(String[] args) {
+		int monthlyWage=0;
 		int dailyWage=0;
+		int day=0;
+		for(day=0; day <NUMBER_OF_WORKING_DAYS;day++)
+		{
 		double empCheck = Math.floor(Math.random()*10)%3;
 		
 		 switch ((int)empCheck)
@@ -17,16 +22,18 @@ public class EmplyoeeWage {
 		  case IS_EMP_PRESENT:
 			  System.out.println("emplyoee is present full time");
 			  dailyWage=FULL_DAY_HOUR * WAGE_PER_HOUR;
-			  System.out.println("emplyoe wage is:"+dailyWage);
 			  break;
 		  case IS_PART_TIME:	  
 			  System.out.println("emplyoee is present part time");
 			  dailyWage=PART_TIME_HOUR * WAGE_PER_HOUR;
-			  System.out.println("emplyoe wage is:"+dailyWage);
 			  break;
 		default:
 			  System.out.println("emplyoee is absent");
 			break;
 		}
-	}
+		 monthlyWage= monthlyWage + dailyWage;
+		 System.out.println("emplyoe wage is:"+dailyWage);
+	  }
+		System.out.println("emplyoe wage per month:"+ monthlyWage);
+   }
 }
