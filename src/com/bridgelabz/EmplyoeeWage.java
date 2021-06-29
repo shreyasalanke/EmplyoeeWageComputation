6d352394dc6d7d3157e52cc42c7e0568b9a31498
shreyasalanke <shreyasalanke97@gmail.com>
2021-06-29 09:34:19 +0530

    ADDED: calculate emplyoee wage per day

--- src/com/bridgelabz/EmplyoeeWage.java
+++ src/com/bridgelabz/EmplyoeeWage.java
@@ -3,18 +3,22 @@ package com.bridgelabz;
 public class EmplyoeeWage {
 	
 	public static final int IS_EMP_PRESENT= 1;
+	public static final int WAGE_PER_HOUR= 20;
+	public static final int FULL_DAY_HOUR= 8;
 	
 	public static void main(String[] args) {
-		
+		int dailyWage=0;
 		double empCheck = Math.floor(Math.random()*10)%2;
 		if(empCheck == 1)
 		{
 			System.out.println("emplyoee is present");
+			dailyWage=FULL_DAY_HOUR * WAGE_PER_HOUR;
 		}
 		else
 		{
 			System.out.println("emplyoee is absent");
 		}
+		System.out.println("daily emplyoee wage is :"+dailyWage);
 	}
 
 }
