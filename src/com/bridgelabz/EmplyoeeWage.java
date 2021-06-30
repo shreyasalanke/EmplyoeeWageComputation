e41e3dab31f6d54fab7ff2f46e97cfd0f194c8cd
shreyasalanke <shreyasalanke97@gmail.com>
2021-06-30 17:28:31 +0530

    MODIFIED: to calculate emp wage of multiple company

--- src/com/bridgelabz/EmplyoeeWage.java
+++ src/com/bridgelabz/EmplyoeeWage.java
@@ -4,44 +4,38 @@ public class EmplyoeeWage {
 	
 	public static final int IS_EMP_PRESENT=1;
 	public static final int IS_PART_TIME=2;
-	public static final int WAGE_PER_HOUR=20;
-	public static final int NUMBER_OF_WORKING_DAYS=20;
-	public static final int MAX_WORK_HOURS=100;
+	
+	
+	public static void calculateEmpWage(int WAGE_PER_HOUR, int MAX_HRS_ALLOWED, int NUM_OF_WROKING_DAY , String company) {
+		int monthlyEmpWage = 0;
+		int totalWorkingDays = 0;
+		int totalEmpHrs = 0;		
+		while (totalEmpHrs < MAX_HRS_ALLOWED && totalWorkingDays < NUM_OF_WROKING_DAY)
+		{
+			totalWorkingDays++ ;
+			int dailywage = 0;
+			int empHrs = 0;
+			double empCheck = Math.floor(Math.random() * 10) % 3;
+			switch ((int) empCheck) {
+			case IS_EMP_PRESENT:
+				empHrs= 8;
+				break;
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			default:
+			}
+			totalEmpHrs = totalEmpHrs + empHrs;
+			dailywage = WAGE_PER_HOUR * empHrs ;
+			monthlyEmpWage = monthlyEmpWage + dailywage ;			
+		}
+		System.out.println("monthly wage : " +monthlyEmpWage+" of "+company);
+		System.out.println("total Hrs : " +totalEmpHrs);
+	}
 	
 	public static void main(String[] args) 
 	{
-		int totalWorkingDays=0;
-		int totalEmplyoeeHour=0;
-		int monthlyWage=0;
-		int empHrs=0;
-	
-		while(totalEmplyoeeHour < MAX_WORK_HOURS && totalWorkingDays < NUMBER_OF_WORKING_DAYS )
-		{
-			
-		totalWorkingDays++;
-		int dailyWage=0;
-	    
-		double empCheck = Math.floor(Math.random()*10)%3;
-		
-		 switch ((int)empCheck)
-		 {
-		  case IS_EMP_PRESENT:
-			  empHrs=8;
-			  break;
-		  case IS_PART_TIME:	  
-			  empHrs=4;
-			  break;
-		default:
-			  empHrs=0;
-			  System.out.println("emplyoee is absent");
-		}
-		 totalEmplyoeeHour= totalEmplyoeeHour + empHrs;
-			dailyWage = WAGE_PER_HOUR * empHrs ;
-			monthlyWage = monthlyWage + dailyWage ;
-			
-			System.out.println("Emplyoee Wage is : "+dailyWage);
-		}
-		System.out.println("monthly wage : " +monthlyWage);
-		System.out.println("total Hrs : " +totalEmplyoeeHour);
+		calculateEmpWage(20, 100, 20,"Dmart");
+		calculateEmpWage(15, 90, 20,"Relience");
 	}
 }
